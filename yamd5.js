dd75bed1560ff96411f51b6354fb44983671be24
gorhill <rhill@raymondhill.net>
2014-12-28 14:40:22 -0500

    more specialized = one less variable

--- yamd5.js
+++ yamd5.js
@@ -76,24 +76,87 @@ THE SOFTWARE.
      */
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
-    var ff = function(a, b, c, d, x, s, t) {
+
+    //var ff = function(a, b, c, d, x, s, t) {
+    //    a += (b & c | ~b & d) + x + t;
+    //    return ((a << s | a >>> (32 - s)) + b) | 0;
+    //};
+    var ff7 = function(a, b, c, d, x, t) {
         a += (b & c | ~b & d) + x + t;
-        return ((a << s | a >>> (32 - s)) + b) | 0;
+        return ((a << 7 | a >>> 25) + b) | 0;
     };
-
-    var gg = function(a, b, c, d, x, s, t) {
+    var ff12 = function(a, b, c, d, x, t) {
+        a += (b & c | ~b & d) + x + t;
+        return ((a << 12 | a >>> 20) + b) | 0;
+    };
+    var ff17 = function(a, b, c, d, x, t) {
+        a += (b & c | ~b & d) + x + t;
+        return ((a << 17 | a >>> 15) + b) | 0;
+    };
+    var ff22 = function(a, b, c, d, x, t) {
+        a += (b & c | ~b & d) + x + t;
+        return ((a << 22 | a >>> 10) + b) | 0;
+    };
+    //var gg = function(a, b, c, d, x, s, t) {
+    //    a += (b & d | c & ~d) + x + t;
+    //    return ((a << s | a >>> (32 - s)) + b) | 0;
+    //};
+    var gg5 = function(a, b, c, d, x, t) {
         a += (b & d | c & ~d) + x + t;
-        return ((a << s | a >>> (32 - s)) + b) | 0;
+        return ((a << 5 | a >>> 27) + b) | 0;
     };
-
-    var hh = function(a, b, c, d, x, s, t) {
+    var gg9 = function(a, b, c, d, x, t) {
+        a += (b & d | c & ~d) + x + t;
+        return ((a << 9 | a >>> 23) + b) | 0;
+    };
+    var gg14 = function(a, b, c, d, x, t) {
+        a += (b & d | c & ~d) + x + t;
+        return ((a << 14 | a >>> 18) + b) | 0;
+    };
+    var gg20 = function(a, b, c, d, x, t) {
+        a += (b & d | c & ~d) + x + t;
+        return ((a << 20 | a >>> 12) + b) | 0;
+    };
+    //var hh = function(a, b, c, d, x, s, t) {
+    //    a += (b ^ c ^ d) + x + t;
+    //    return ((a << s | a >>> (32 - s)) + b) | 0;
+    //};
+    var hh4 = function(a, b, c, d, x, t) {
         a += (b ^ c ^ d) + x + t;
-        return ((a << s | a >>> (32 - s)) + b) | 0;
+        return ((a << 4 | a >>> 28) + b) | 0;
+    };
+    var hh11 = function(a, b, c, d, x, t) {
+        a += (b ^ c ^ d) + x + t;
+        return ((a << 11 | a >>> 21) + b) | 0;
+    };
+    var hh16 = function(a, b, c, d, x, t) {
+        a += (b ^ c ^ d) + x + t;
+        return ((a << 16 | a >>> 16) + b) | 0;
+    };
+    var hh23 = function(a, b, c, d, x, t) {
+        a += (b ^ c ^ d) + x + t;
+        return ((a << 23 | a >>> 9) + b) | 0;
     };
 
-    var ii = function(a, b, c, d, x, s, t) {
+    //var ii = function(a, b, c, d, x, s, t) {
+    //    a += (c ^ (b | ~d)) + x + t;
+    //    return ((a << s | a >>> (32 - s)) + b) | 0;
+    //};
+    var ii6 = function(a, b, c, d, x, t) {
         a += (c ^ (b | ~d)) + x + t;
-        return ((a << s | a >>> (32 - s)) + b) | 0;
+        return ((a << 6 | a >>> 26) + b) | 0;
+    };
+    var ii10 = function(a, b, c, d, x, t) {
+        a += (c ^ (b | ~d)) + x + t;
+        return ((a << 10 | a >>> 22) + b) | 0;
+    };
+    var ii15 = function(a, b, c, d, x, t) {
+        a += (c ^ (b | ~d)) + x + t;
+        return ((a << 15 | a >>> 17) + b) | 0;
+    };
+    var ii21 = function(a, b, c, d, x, t) {
+        a += (c ^ (b | ~d)) + x + t;
+        return ((a << 21 | a >>> 11) + b) | 0;
     };
 
     var md5cycle = function(x, k) {
@@ -102,73 +165,73 @@ THE SOFTWARE.
             c = x[2],
             d = x[3];
 
-        a = ff(a, b, c, d, k[0], 7, -680876936);
-        d = ff(d, a, b, c, k[1], 12, -389564586);
-        c = ff(c, d, a, b, k[2], 17, 606105819);
-        b = ff(b, c, d, a, k[3], 22, -1044525330);
-        a = ff(a, b, c, d, k[4], 7, -176418897);
-        d = ff(d, a, b, c, k[5], 12, 1200080426);
-        c = ff(c, d, a, b, k[6], 17, -1473231341);
-        b = ff(b, c, d, a, k[7], 22, -45705983);
-        a = ff(a, b, c, d, k[8], 7, 1770035416);
-        d = ff(d, a, b, c, k[9], 12, -1958414417);
-        c = ff(c, d, a, b, k[10], 17, -42063);
-        b = ff(b, c, d, a, k[11], 22, -1990404162);
-        a = ff(a, b, c, d, k[12], 7, 1804603682);
-        d = ff(d, a, b, c, k[13], 12, -40341101);
-        c = ff(c, d, a, b, k[14], 17, -1502002290);
-        b = ff(b, c, d, a, k[15], 22, 1236535329);
+        a =  ff7(a, b, c, d,  k[0], -680876936);
+        d = ff12(d, a, b, c,  k[1], -389564586);
+        c = ff17(c, d, a, b,  k[2], 606105819);
+        b = ff22(b, c, d, a,  k[3], -1044525330);
+        a =  ff7(a, b, c, d,  k[4], -176418897);
+        d = ff12(d, a, b, c,  k[5], 1200080426);
+        c = ff17(c, d, a, b,  k[6], -1473231341);
+        b = ff22(b, c, d, a,  k[7], -45705983);
+        a =  ff7(a, b, c, d,  k[8], 1770035416);
+        d = ff12(d, a, b, c,  k[9], -1958414417);
+        c = ff17(c, d, a, b, k[10], -42063);
+        b = ff22(b, c, d, a, k[11], -1990404162);
+        a =  ff7(a, b, c, d, k[12], 1804603682);
+        d = ff12(d, a, b, c, k[13], -40341101);
+        c = ff17(c, d, a, b, k[14], -1502002290);
+        b = ff22(b, c, d, a, k[15], 1236535329);
 
-        a = gg(a, b, c, d, k[1], 5, -165796510);
-        d = gg(d, a, b, c, k[6], 9, -1069501632);
-        c = gg(c, d, a, b, k[11], 14, 643717713);
-        b = gg(b, c, d, a, k[0], 20, -373897302);
-        a = gg(a, b, c, d, k[5], 5, -701558691);
-        d = gg(d, a, b, c, k[10], 9, 38016083);
-        c = gg(c, d, a, b, k[15], 14, -660478335);
-        b = gg(b, c, d, a, k[4], 20, -405537848);
-        a = gg(a, b, c, d, k[9], 5, 568446438);
-        d = gg(d, a, b, c, k[14], 9, -1019803690);
-        c = gg(c, d, a, b, k[3], 14, -187363961);
-        b = gg(b, c, d, a, k[8], 20, 1163531501);
-        a = gg(a, b, c, d, k[13], 5, -1444681467);
-        d = gg(d, a, b, c, k[2], 9, -51403784);
-        c = gg(c, d, a, b, k[7], 14, 1735328473);
-        b = gg(b, c, d, a, k[12], 20, -1926607734);
+        a =  gg5(a, b, c, d,  k[1], -165796510);
+        d =  gg9(d, a, b, c,  k[6], -1069501632);
+        c = gg14(c, d, a, b, k[11], 643717713);
+        b = gg20(b, c, d, a,  k[0], -373897302);
+        a =  gg5(a, b, c, d,  k[5], -701558691);
+        d =  gg9(d, a, b, c, k[10], 38016083);
+        c = gg14(c, d, a, b, k[15], -660478335);
+        b = gg20(b, c, d, a,  k[4], -405537848);
+        a =  gg5(a, b, c, d,  k[9], 568446438);
+        d =  gg9(d, a, b, c, k[14], -1019803690);
+        c = gg14(c, d, a, b,  k[3], -187363961);
+        b = gg20(b, c, d, a,  k[8], 1163531501);
+        a =  gg5(a, b, c, d, k[13], -1444681467);
+        d =  gg9(d, a, b, c,  k[2], -51403784);
+        c = gg14(c, d, a, b,  k[7], 1735328473);
+        b = gg20(b, c, d, a, k[12], -1926607734);
 
-        a = hh(a, b, c, d, k[5], 4, -378558);
-        d = hh(d, a, b, c, k[8], 11, -2022574463);
-        c = hh(c, d, a, b, k[11], 16, 1839030562);
-        b = hh(b, c, d, a, k[14], 23, -35309556);
-        a = hh(a, b, c, d, k[1], 4, -1530992060);
-        d = hh(d, a, b, c, k[4], 11, 1272893353);
-        c = hh(c, d, a, b, k[7], 16, -155497632);
-        b = hh(b, c, d, a, k[10], 23, -1094730640);
-        a = hh(a, b, c, d, k[13], 4, 681279174);
-        d = hh(d, a, b, c, k[0], 11, -358537222);
-        c = hh(c, d, a, b, k[3], 16, -722521979);
-        b = hh(b, c, d, a, k[6], 23, 76029189);
-        a = hh(a, b, c, d, k[9], 4, -640364487);
-        d = hh(d, a, b, c, k[12], 11, -421815835);
-        c = hh(c, d, a, b, k[15], 16, 530742520);
-        b = hh(b, c, d, a, k[2], 23, -995338651);
+        a =  hh4(a, b, c, d,  k[5], -378558);
+        d = hh11(d, a, b, c,  k[8], -2022574463);
+        c = hh16(c, d, a, b, k[11], 1839030562);
+        b = hh23(b, c, d, a, k[14], -35309556);
+        a =  hh4(a, b, c, d,  k[1], -1530992060);
+        d = hh11(d, a, b, c,  k[4], 1272893353);
+        c = hh16(c, d, a, b,  k[7], -155497632);
+        b = hh23(b, c, d, a, k[10], -1094730640);
+        a =  hh4(a, b, c, d, k[13], 681279174);
+        d = hh11(d, a, b, c,  k[0], -358537222);
+        c = hh16(c, d, a, b,  k[3], -722521979);
+        b = hh23(b, c, d, a,  k[6], 76029189);
+        a =  hh4(a, b, c, d,  k[9], -640364487);
+        d = hh11(d, a, b, c, k[12], -421815835);
+        c = hh16(c, d, a, b, k[15], 530742520);
+        b = hh23(b, c, d, a,  k[2], -995338651);
 
-        a = ii(a, b, c, d, k[0], 6, -198630844);
-        d = ii(d, a, b, c, k[7], 10, 1126891415);
-        c = ii(c, d, a, b, k[14], 15, -1416354905);
-        b = ii(b, c, d, a, k[5], 21, -57434055);
-        a = ii(a, b, c, d, k[12], 6, 1700485571);
-        d = ii(d, a, b, c, k[3], 10, -1894986606);
-        c = ii(c, d, a, b, k[10], 15, -1051523);
-        b = ii(b, c, d, a, k[1], 21, -2054922799);
-        a = ii(a, b, c, d, k[8], 6, 1873313359);
-        d = ii(d, a, b, c, k[15], 10, -30611744);
-        c = ii(c, d, a, b, k[6], 15, -1560198380);
-        b = ii(b, c, d, a, k[13], 21, 1309151649);
-        a = ii(a, b, c, d, k[4], 6, -145523070);
-        d = ii(d, a, b, c, k[11], 10, -1120210379);
-        c = ii(c, d, a, b, k[2], 15, 718787259);
-        b = ii(b, c, d, a, k[9], 21, -343485551);
+        a =  ii6(a, b, c, d,  k[0], -198630844);
+        d = ii10(d, a, b, c,  k[7], 1126891415);
+        c = ii15(c, d, a, b, k[14], -1416354905);
+        b = ii21(b, c, d, a,  k[5], -57434055);
+        a =  ii6(a, b, c, d, k[12], 1700485571);
+        d = ii10(d, a, b, c,  k[3], -1894986606);
+        c = ii15(c, d, a, b, k[10], -1051523);
+        b = ii21(b, c, d, a,  k[1], -2054922799);
+        a =  ii6(a, b, c, d,  k[8], 1873313359);
+        d = ii10(d, a, b, c, k[15], -30611744);
+        c = ii15(c, d, a, b,  k[6], -1560198380);
+        b = ii21(b, c, d, a, k[13], 1309151649);
+        a =  ii6(a, b, c, d,  k[4], -145523070);
+        d = ii10(d, a, b, c, k[11], -1120210379);
+        c = ii15(c, d, a, b,  k[2], 718787259);
+        b = ii21(b, c, d, a,  k[9], -343485551);
 
         x[0] = (a + x[0]) | 0;
         x[1] = (b + x[1]) | 0;
